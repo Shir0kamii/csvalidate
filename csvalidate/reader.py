d31990b4cdaffd118ed5047a76fb3e838f6e2def
Alexandre Bonnetain <alexandre.bonnetain@1000mercis.com>
2017-01-12 22:23:45 +0100

    Fix python2 compatibility issue

--- csvalidate/reader.py
+++ csvalidate/reader.py
@@ -7,10 +7,15 @@ class ValidatedReader(DictReader):
     schema = None
 
     def __next__(self):
-        obj = super(ValidatedReader, self).__next__()
+        obj = DictReader.__next__(self)
         if self.schema:
             data, errors = self.schema.load(obj)
             if errors:
                 raise ValidationError(errors, data=data)
             obj = data
         return obj
+
+    def next(self):
+        if not hasattr(DictReader, "__next__"):
+            DictReader.__next__ = DictReader.next
+        return self.__next__()
