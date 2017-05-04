47845c9c857887547ccf4b69e3dcd37370352033
Alexandre Bonnetain <shir0kamii@gmail.com>
2017-05-04 18:17:12 +0200

    Bump version: 1.1.0 → 1.1.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 
 setup(
     name="csvalidate",
-    version="1.1.0",
+    version="1.1.1",
     author="Shir0kamii",
     author_email="shir0kamii@gmail.com",
     description="CSV reader and writer with validation",
