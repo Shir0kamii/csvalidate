c9e608b776b5766509b83c4f0644bc2274cc14f3
Alexandre Bonnetain <shir0kamii@gmail.com>
2018-08-16 17:06:09 +0000

    Add license in setup.py

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
     platforms="any",
     packages=["csvalidate"],
     classifiers=[
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.4",
